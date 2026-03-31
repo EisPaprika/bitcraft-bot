dd4ab0dd3fe67862441f422693d57f950b3de1c9
Decabits <271550167+Decabits-Git@users.noreply.github.com>
2026-03-31 13:01:28 -0400

    refactor function to distinguish between normal and large crafts; move footer logic to be contained by function

--- src/utils/embeds/embed_builder.py
+++ src/utils/embeds/embed_builder.py
@@ -3,18 +3,22 @@ import datetime
 
 '''
 Function - new_craft_embed
-Inputs   - **kwargs      : Dynamic number of key/value pairs to insert into Embed description
+Inputs   - [bool] is_large_craft : True if a craft is large, False if not
+           [dict] embed_description : Custom key-value pairs containing the description for the Embed
+           [str] footer : The text to be inserted into the footer of the embed object, conventionally an entityId
 Outputs  - [Embed] embed : The Embed object to send in a Discord message
 Purpose  - Create and configure the Discord Embed object for crafting alerts
 '''
-def new_craft_embed(**kwargs):
+def new_craft_embed(is_large_craft: bool, embed_description: dict, footer: str):
     relative_timestamp = datetime.datetime.now(datetime.timezone.utc)
-    embed = discord.Embed(color=0xFFFFFF, title=":loudspeaker: NEW LARGE CRAFT :loudspeaker:", timestamp=relative_timestamp)
+    title = ":loudspeaker: NEW LARGE CRAFT :loudspeaker:" if is_large_craft else ":tools: New Craft :tools:"
+    embed = discord.Embed(color=0xFFFFFF, title=title, timestamp=relative_timestamp)
     embed.set_author(name="LuniBot - Myralune Craft Alert")
 
     description = ""
-    for k, val in kwargs.items():
-        description += f'**{k}:** {val}\n'
+    for key, value in embed_description.items():
+        description += f'**{key}:** {value}\n'
             
     embed.description = description
+    embed.set_footer(text=footer)
     return embed
